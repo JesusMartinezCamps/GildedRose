70fbd33c8b2b0753b05e41a214c9c47b6ed16e0e
JesusMartinezCamps <gsus.martinez97@gmail.com>
2016-12-15 18:47:52 +0100

    Test comprobados

---
+++ sulfurasTest.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+##Caso test Sulfuras
+from clases import *
+
+if __name__ == '__main__':
+
+	item = Sulfuras("Sulfuras, Hand of Ragnaros", 0, 70)
+	print(item)
+
+	for dia in range(1, 10):
+		item.update_quality()
+		print(item)
+	
