efc8d15763700b2708a3ed10e33da696119366c6
JesusMartinezCamps <gsus.martinez97@gmail.com>
2016-12-19 11:17:03 +0100

    cambios menores

--- test_NormalItem.py
+++ test_NormalItem.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
+##Caso test de un item normal
 from clases import *
 
 if __name__ == '__main__':
 
     item = NormalItem("Elixir of the Mongoose", 5, 7)
-    # chequeo herencia __repr__
     print(item)
-    # test update_quality
+    
     for dia in range(1, 10):
         item.update_quality()
         print(item)
